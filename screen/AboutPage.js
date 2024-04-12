2f6b4fe9d82330967fe2884cee0398fdd9ac4c46
kenethlompon <lomponkeneth02@gmail.com>
2024-04-12 17:33:34 +0800

    8

--- screen/AboutPage.js
+++ screen/AboutPage.js
@@ -1,11 +1,61 @@
-// AboutPage.js
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 
 const AboutPage = () => {
   return (
-    <Text>About Us Page</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/1a.jpg')}
+            style={styles.image}
+          />
+          <Text style={styles.title}>Project Manager</Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/2a.jpg')}
+            style={styles.image}
+          />
+          <Text style={styles.title}>Backend</Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/3a.jpg')}
+            style={styles.image}
+          />
+          <Text style={styles.title}>Frontend</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+});
 
 export default AboutPage;
